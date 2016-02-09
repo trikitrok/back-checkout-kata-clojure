2a7b560a14555b4e75649c00d659b2116c7500d9
trikitrok <trikitrok@gmail.com>
2016-02-09 19:44:24 +0100

    Extracted helper

--- src/back_checkout/core.clj
+++ src/back_checkout/core.clj
@@ -6,14 +6,16 @@
 (defn unit-price [prices-by-good good-sku]
   (get-in prices-by-good [good-sku :unit-price] 0))
 
+(defn special-price [prices-by-good [good-sku amount]]
+  (get-in prices-by-good [good-sku :special-prices amount]))
+
 (defn price [prices-by-good goods]
   (->> goods
        (map sku)
        frequencies
        (map #(if-let
-              [special-price (get-in prices-by-good
-                                     [(first %) :special-prices (second %)])]
-              special-price
+              [price (special-price prices-by-good %)]
+              price
               (* (second %) (unit-price prices-by-good
                                         (first %)))))
        (reduce +)))
