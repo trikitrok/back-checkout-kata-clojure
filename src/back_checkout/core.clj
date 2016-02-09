3d4292689d7c8233298ffbadd453642f3c209338
trikitrok <trikitrok@gmail.com>
2016-02-09 18:57:45 +0100

    Extracted a helper

--- src/back_checkout/core.clj
+++ src/back_checkout/core.clj
@@ -1,6 +1,9 @@
 (ns back-checkout.core)
 
+(defn sku [c]
+  (keyword (str c)))
+
 (defn price [prices-by-good goods]
   (if (empty? goods)
     0
-    (prices-by-good (keyword (str (first goods))))))
+    (prices-by-good (sku (first goods)))))
